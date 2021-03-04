fe3aa9d2d1da774121b051f4381b83568d9e543b
sarahmardjuki <77697165+sarahmardjuki@users.noreply.github.com>
2021-03-03 23:21:02 -0800

    Data validate for valid symbols

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -4,27 +4,45 @@ import requests
 import json
 import os
 import csv
+from dotenv import load_dotenv
+
+load_dotenv() # load contents of .env file to environment
 
 # functions
 def to_usd(my_price):
     return "${0:,.2f}".format(my_price)
 
+# INPUTS
+api_key = os.environ.get("ALPHAVANTAGE_API_KEY")
+
 # USER INPUT
 while True:
     stock = input("Please enter a stock or cryptocurrency symbol: ")
+
+    request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={stock}&apikey={api_key}"
+
+    response = requests.get(request_url)
+    parsed_response = json.loads(response.text)
+    for key, value in parsed_response.items():
+        if key == "Error Message":
+            invalidsymbol = 1
+            break 
+        else:
+            invalidsymbol = 0
 
     # data validate
     if len(stock) > 6:
         print("Hm, that doesn't look like a valid symbol. Please try again!")
     elif stock.isnumeric() == True:
         print("Hm, that doesn't look like a valid symbol. Please try again!")
+    elif invalidsymbol == 1:
+        print("Hm, that doesn't look like a valid symbol. Please try again!")
     else:
         break
 
-request_url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&apikey=demo"
 
-response = requests.get(request_url)
-parsed_response = json.loads(response.text)
+
+
 
 # request at:
 request_at = datetime.now()
