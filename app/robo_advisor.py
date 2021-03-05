0e3d1d63b0c2558a5f0e47b7d8b7a9e03564c789
sarahmardjuki <77697165+sarahmardjuki@users.noreply.github.com>
2021-03-04 16:42:19 -0800

    Update recommendation algorithm

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -55,20 +55,50 @@ dates.sort(key=lambda date:datetime.strptime(date,"%Y-%m-%d"), reverse = True)
 
 latest_day = dates[0]
 latest_close = tsd[latest_day]["4. close"]
+latest_volume = tsd[latest_day]["5. volume"]
 
-# recent high and low
-
-
+# recent high and low and volume
 high_prices = []
 low_prices = []
+high_volumes = []
 for d in tsd:
     high_price = tsd[d]["2. high"]
     low_price = tsd[d]["3. low"]
+    high_volume = tsd[d]["5. volume"]
     high_prices.append(high_price)
     low_prices.append(low_price)
+    high_volumes.append(high_volume)
 
 recent_high = max(high_prices)
 recent_low = min(high_prices)
+recent_high_volume = max(high_volumes)
+
+
+
+# RECOMMENDATION AND REASON
+
+# if close price is > recent high and volume is > recent high volume, then BUY
+if (float(latest_close) > float(recent_high)) and (int(latest_volume) > int(recent_high_volume)):
+    latest_volume = "{:,}".format(int(latest_volume))
+    recent_high_volume = "{:,}".format(int(recent_high_volume))
+    recommendation = "BUY"
+    reason = f"{stock.upper()}'s latest close of {to_usd(float(latest_close))} is greater than its recent high of {to_usd(float(recent_high))}, and its most recent volume of {latest_volume} is also greater than its recent high volume of {recent_high_volume}. This indicates that demand will likely continue pushing the price up."
+elif (float(latest_close) > float(recent_high)) and (int(latest_volume) < int(recent_high_volume)):
+    latest_volume = "{:,}".format(int(latest_volume))
+    recent_high_volume = "{:,}".format(int(recent_high_volume))
+    recommendation = "DON'T BUY"
+    reason = f"{stock.upper()}'s latest close of {to_usd(float(latest_close))} is greater than its recent high of {to_usd(float(recent_high))}, but its most recent volume of {latest_volume} is not greater than its recent high volume of {recent_high_volume}. This indicates that it is likely not demand pushing the price up, and the price may not continue increasing."
+elif (float(latest_close) < float(recent_high)) and (int(latest_volume) > int(recent_high_volume)):
+    latest_volume = "{:,}".format(int(latest_volume))
+    recent_high_volume = "{:,}".format(int(recent_high_volume))
+    recommendation = "DON'T BUY"
+    reason = f"Even though {stock.upper()}'s most recent volume of {latest_volume} is greater than its recent high volume of {recent_high_volume}, its latest close of {to_usd(float(latest_close))} is less than its recent high of {to_usd(float(recent_high))}."
+elif (float(latest_close) < float(recent_high)) and (int(latest_volume) < int(recent_high_volume)):
+    latest_volume = "{:,}".format(int(latest_volume))
+    recent_high_volume = "{:,}".format(int(recent_high_volume))
+    recommendation = "DON'T BUY"
+    reason = f"{stock.upper()}'s latest close of {to_usd(float(latest_close))} is not greater than its recent high of {to_usd(float(recent_high))}, and its most recent volume of {latest_volume} is not greater than its recent high volume of {recent_high_volume}. This indicates that there is not much interest in this stock, and the price may not increase in the near future."
+
 
 
 # WRITE TO CSV
@@ -99,8 +129,8 @@ print(f"LATEST CLOSE: {to_usd(float(latest_close))}")
 print(f"RECENT HIGH: {to_usd(float(recent_high))}")
 print(f"RECENT LOW: {to_usd(float(recent_low))}")
 print("-------------------------")
-print(f"RECOMMENDATION: ")
-print(f"RECOMMENDATION REASON: ")
+print(f"RECOMMENDATION: {recommendation}")
+print(f"RECOMMENDATION REASON: {reason}")
 print("-------------------------")
 print("WRITING TO CSV...")
 print("-------------------------")
